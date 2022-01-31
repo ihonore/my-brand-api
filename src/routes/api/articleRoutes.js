21ff82f358f75a11b63c68c13616a4026c8ee238
Honore IRADUKUNDA <ihonore01@gmail.com>
2022-01-31 12:14:10 +0200

    added authentication, validation and user profile update

--- src/routes/api/articleRoutes.js
+++ src/routes/api/articleRoutes.js
@@ -1,28 +1,24 @@
 import express from 'express'
 import { ArticleController } from './../../controllers/articleController.js'
 
-
 import multer from 'multer';
+import { fileFilter } from '../../helpers/fileFilter.js';
+import { authenticate } from '../../middlewares/authenticate.js';
+import { articleValidation } from '../../validations/articleValidation/article.validation.js';
 
 const route = express.Router()
 
 const storage = multer.diskStorage({});
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image')) {
-        cb(null, true);
-    } else {
-        cb('invalid image file!', false);
-    }
-};
+
 
 const uploads = multer({ storage, fileFilter });
 const articleControllers = new ArticleController()
-route.post('/', uploads.single('image'), articleControllers.createArticle)
+
+route.post('/', authenticate, uploads.single('image'), articleValidation, articleControllers.createArticle)
 route.get('/', articleControllers.getAllArticles)
 route.get('/:id', articleControllers.getArticle)
-route.put('/:id', uploads.single('image'), articleControllers.updateArticle)
-route.patch('/:id', articleControllers.updateArticle)
-route.delete('/:id', articleControllers.deleteArticle)
+route.patch('/:id', authenticate, uploads.single('image'), articleControllers.updateArticle)
+route.delete('/:id', authenticate, articleControllers.deleteArticle)
 
 
 export default route
